b44de41547a7264f1e41c94277604db60a54658d
raman@techvisionit.com <raman@techvisionit.com>
2018-05-13 09:49:58 +0530

    maven code

---
+++ src/main/java/testMaven/testMaven/App.java
@@ -0,0 +1,13 @@
+package testMaven.testMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
